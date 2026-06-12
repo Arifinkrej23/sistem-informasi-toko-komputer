053669337644acaf317277030406a0041173bd25
Arifinkrej23 <arifputra1019@gmail.com>
2026-06-12 09:11:10 +0700

    firsh commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,180 +10,55 @@
 <a class="navbar-brand fw-bold">
 Sistem Penjualan Komputer
 </a>
-
-
-
-
 <a href="/login"
-
 class="btn btn-primary">
-
 Login
-
 </a>
-
-
-
 </div>
-
-
 </nav>
-
-
-
-
-
-
-
 <main class="container mt-5 flex-grow-1">
-
-
-
-
-
 <div class="row align-items-center">
-
-
-
-
-
-
 <div class="col-md-6">
-
-
-
 <h1 class="fw-bold">
-
-
 Sistem Informasi
-
 <br>
-
 Penjualan Toko Komputer
-
-
 </h1>
-
-
-
-
-
 <p class="mt-4 text-muted">
-
-
 Aplikasi pengelolaan penjualan komputer
 untuk mengatur kategori produk, produk,
 transaksi, dan pembayaran.
-
-
 </p>
-
-
-
-
-
 <a href="/login"
-
 class="btn btn-success btn-lg mt-3">
-
-
 Mulai Sekarang
-
-
 </a>
-
-
-
-
 </div>
-
-
-
-
-
-
-
-
-
 <div class="col-md-6">
-
-
-
 <div class="card shadow border-0">
-
-
-
 <div class="card-body">
-
-
-
-
-
 <h3 class="text-center mb-4">
-
 Fitur Sistem
-
 </h3>
-
-
-
-
-
-
 <div class="row">
-
-
-
-
-
 <div class="col-md-6 mb-3">
-
-
 <div class="card h-100 shadow-sm">
-
-
 <div class="card-body text-center">
-
-
 <h5>
-
 Kategori
-
 </h5>
-
-
 <p class="text-muted">
-
 Kelola kategori produk
-
 </p>
-
-
 </div>
-
-
 </div>
-
-
 </div>
-
-
-
-
-
 
 
 <div class="col-md-6 mb-3">
-
-
 <div class="card h-100 shadow-sm">
-
-
 <div class="card-body text-center">
-
-
 <h5>
-
 Produk
 
 </h5>
